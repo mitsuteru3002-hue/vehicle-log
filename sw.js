c8e7989d806836098925848d11d15cd2e85f9e32
mitsuteru3002-hue <mitsuteru.3002@gmail.com>
2026-05-26 09:47:59 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,11 @@
-const CACHE_NAME = 'vehicle-log-v2';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
+// キャッシュを使わない（常に最新版を取得）
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
   );
   self.clients.claim();
 });
-
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request).catch(() => caches.match('./index.html'));
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
